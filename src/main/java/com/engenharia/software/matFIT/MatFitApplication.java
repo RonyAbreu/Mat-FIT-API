f5e9a4f441e1411b40f59223f84966b85ca4db5d
Rony Abreu <ronyelison78@gmail.com>
2024-07-15 10:05:15 -0300

    finalização do projeto

---
+++ src/main/java/com/engenharia/software/matFIT/MatFitApplication.java
@@ -0,0 +1,13 @@
+package com.engenharia.software.matFIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engenharia/software/matFIT/MatFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engenharia.software.matFIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
